ce5c33eca4c8d7cdeb7aeb9fea22bd5efcdc6e3c
antnavarro <antnavarro@106-17.info2>
2022-09-26 09:11:40 +0200

    BRANCH

--- Excersises - Part 2/src/excersise1/Excersise1.java
+++ Excersises - Part 2/src/excersise1/Excersise1.java
@@ -1,9 +1,18 @@
 package excersise1;
+import java.util.Scanner;
 
 public class Excersise1 {
 
-	public static void main(String[] args) {
-		
+	public static void main (String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		final int YEAR;
+		int age, bornIn;
+		System.out.print("How old are you this year? ");
+		age = keyboard.nextDouble();
+		bornIn = YEAR – age;
+		System.out.println("I think you were born in " + BornIn);
+	}
+
 
 	}
 
