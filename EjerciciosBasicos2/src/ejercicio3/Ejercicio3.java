18f9b47210f3a6ad63a787463d6ec4522bc108e8
antnavarro <antnavarro@106-17.info2>
2022-09-28 09:35:45 +0200

    LALFLA

--- EjerciciosBasicos2/src/ejercicio3/Ejercicio3.java
+++ EjerciciosBasicos2/src/ejercicio3/Ejercicio3.java
@@ -12,12 +12,25 @@ public class Ejercicio3 {
 		// Variables
 		long numero;
 		
+		boolean rec;
+		
 		// Scanner
 		final Scanner read = new Scanner(System.in);
 		read.useLocale(Locale.US);
 		
 		// App
+		System.out.print("Indtroduce un numero: ");
 		numero = read.nextLong();
+		
+		// Calc
+		numero %= 2;
+		
+		rec = numero == 0 ? true : false;
+		
+		// Resultado
+		System.out.println("Es el numero par?");
+		System.out.println(rec);
+		
 		
 		read.close();
 		
--- EjerciciosBasicos2/src/ejercicio5/Ejercicio5.java
+++ EjerciciosBasicos2/src/ejercicio5/Ejercicio5.java
@@ -1,29 +1,38 @@
 package ejercicio5;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Ejercicio5 {
-	public void main (String[] args) {
+
+	public static void main(String[] args) {
 		// Variables
 		byte nota1, nota2, nota3;
 		
+		float notamedia;
+
 		// Scanner
 		final Scanner read = new Scanner(System.in);
+		read.useLocale(Locale.US);
 		
+
 		// App
-		System.out.print("Introduce la nota del Primer Trimestre");
+		System.out.print("Introduce la nota del Primer Trimestre: ");
 		nota1 = read.nextByte();
-		
-		System.out.print("Introduce la nota del Segundo Trimestre");
+
+		System.out.print("Introduce la nota del Segundo Trimestre: ");
 		nota2 = read.nextByte();
-		
-		System.out.print("Introduce la nota del Tercer Trimestre");
+
+		System.out.print("Introduce la nota del Tercer Trimestre: ");
 		nota3 = read.nextByte();
+
+		// Calculos
+		notamedia = (nota1 + nota2 + nota3) / 3.0f;
 		
-		System.out.println(nota1 + nota2 + nota3);
-		
+		System.out.println(notamedia);
+
 		read.close();
-		
+
 	}
 
 }
